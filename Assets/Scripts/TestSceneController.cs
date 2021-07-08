5331fb734098125c829639ecd565e0bb2b5215f6
Jack Lumers <kailneedko@gmail.com>
2021-07-08 20:50:46 +0300

    Test scene changes

---
+++ Assets/Scripts/TestSceneController.cs
@@ -0,0 +1,16 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UniversalUnity.Helpers.UI.BaseUiElements;
+
+public class TestSceneController : MonoBehaviour
+{
+    [SerializeField] private BaseInteractableUiElement baseUiElementSwitch;
+    [SerializeField] private BaseUiElement baseUiElement;
+    
+    private void Awake()
+    {
+        baseUiElementSwitch.OnClick 
+            += UniTask.Action
+                (async () => await baseUiElement.EnableOrDisable(!baseUiElement.IsEnabled, true));
+    }
+}
